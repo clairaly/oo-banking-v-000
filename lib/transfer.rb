d13ac09d08c475d275a78c9d023d457ea4c6f05d
Alyssa Clair <clairaly13@gmail.com>
2017-12-08 20:03:01 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,4 +12,23 @@ class Transfer
   def valid?
     sender.valid? && receiver.valid? && sender.balance >= amount
   end
+
+  def execute_transaction
+      if @status == "pending" && valid?
+        sender.deposit(-amount)
+        receiver.deposit(amount)
+        @status = "complete"
+      elsif !valid?
+        @status = "rejected"
+        "Transaction rejected. Please check your account balance."
+      end
+    end
+
+    def reverse_transfer
+    if @status == 'complete'
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = 'reversed'
+    end
+  end
 end
